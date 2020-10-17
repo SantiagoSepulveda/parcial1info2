dcbd15e9bdb2735362d418730bad5db6703605b8
SantiagoSepulveda <santialejo10@gmail.com>
2020-10-17 16:03:54 -0500

    primer commit del codigo donde se inicializan las variables.

---
+++ parcial1info2/main.cpp
@@ -0,0 +1,37 @@
+/*
+PARCIAL 1 INFORMATICA 2.
+Santiago Alejandro Sepulveda Palacio.
+cc: 1022097969
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float g= 9.8;
+    int Ho; int Hd; int d; int VoD; int alfa;
+
+    cout << "Este programa realiza una serie de calculos en base al movimiento parabolico." << endl;
+
+    cout << "ingrese la distancia d entre los dos canones: " << endl;
+    cin >> d;
+
+    cout << "ingrese la altura Ho del canon ofensivo: " << endl;
+    cin >> Ho;
+
+    cout << "ingrese la altura Hd del canon defensivo: " << endl;
+    cin >> Hd;
+
+    cout << "ingrese la velocidad inicial del canon defensivo: " << endl;
+    cin >> VoD;
+
+    cout << "ingrese el angulo alfa del canon ofensivo: " << endl;
+    cin >> alfa;
+
+
+
+
+    return 0;
+}
